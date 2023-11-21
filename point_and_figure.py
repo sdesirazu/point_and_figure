a976457e1a61fe51680d575f6b310d79fe197de4
Srinivas Desirazu <srinivas.desirazu@transport.nsw.gov.au>
2023-11-21 15:26:09 +1100

    debugging 9

--- point_and_figure.py
+++ point_and_figure.py
@@ -171,8 +171,9 @@ class PointAndFigure:
         if self.returnChart == 1:
             return chart
 
-        print("printing debug values")
-        for i in row:
+        print("printing debug values "+col)
+        for i in range(row):
+            print(i)
             print(grid[i][col])
 
         print("finished printing debug values")
