54f40de7c01cab638f678e88f6b1ad8c2a9569bc
Srinivas Desirazu <srinivas.desirazu@transport.nsw.gov.au>
2023-11-24 20:56:56 +1100

    mplfinance and the debugging continue 10

--- finance.py
+++ finance.py
@@ -8,8 +8,8 @@ startDate = "2023-01-01"
 endDate = None
 
 hist = instrument.history(start=startDate, end=endDate)
-df = pd.DataFrame({'Date':hist['Date'].values, 'Open': hist['Open'].values, 'High': hist['High'].values, 'Low': hist['Low'].values, 'Close': hist['Close'].values, 'Volume': hist['Volume'].values})
-
+df = pd.DataFrame({'Date':hist['Date'].index, 'Open': hist['Open'].values, 'High': hist['High'].values, 'Low': hist['Low'].values, 'Close': hist['Close'].values, 'Volume': hist['Volume'].values})
+df = df.set_index('Date')
 close_prices = df['Close']
 dates = df['Date']
 
