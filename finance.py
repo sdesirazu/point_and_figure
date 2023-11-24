72d4b72c4fa1df765f13741df85651fedce295ed
Srinivas Desirazu <srinivas.desirazu@transport.nsw.gov.au>
2023-11-24 21:15:21 +1100

    mplfinance and the debugging continue 13

--- finance.py
+++ finance.py
@@ -7,9 +7,10 @@ instrument = yf.Ticker("MSFT")
 startDate = "2023-01-01"
 endDate = None
 
-hist = instrument.history(start=startDate, end=endDate)
-df = pd.DataFrame({'Date':hist['Close'].index, 'Open': hist['Open'].values, 'High': hist['High'].values, 'Low': hist['Low'].values, 'Close': hist['Close'].values, 'Volume': hist['Volume'].values})
+#hist = instrument.history(start=startDate, end=endDate)
+#df = pd.DataFrame({'Date':hist['Close'].index, 'Open': hist['Open'].values, 'High': hist['High'].values, 'Low': hist['Low'].values, 'Close': hist['Close'].values, 'Volume': hist['Volume'].values})
 
+df = yf.download("MSFT", period='6mo')
 close_prices = df['Close']
 dates = df['Date']
 
