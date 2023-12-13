16d36db48d4ba087311e7b41c2d08850092a3f6f
Srinivas Desirazu <srinivas.desirazu@transport.nsw.gov.au>
2023-12-13 13:28:44 +1100

    debugging

--- thirdspreadsheet.py
+++ thirdspreadsheet.py
@@ -66,9 +66,7 @@ for ticker in col:
 
         price=float(price)
         df_closest = df.iloc[(df["strike"]-price).abs().argsort()[:1]]
-        print(price)
         closest_value = df_closest["strike"].tolist()[0]
-        print(closest_value)
         li.append(closest_value)
         li.append(df_closest["bid"].tolist()[0])
         li.append(df_closest["ask"].tolist()[0])
