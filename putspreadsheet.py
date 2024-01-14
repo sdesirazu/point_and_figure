87e5e0b5da397b0c208328f03ffc3e9da8081a41
sdesirazu <99233525+sdesirazu@users.noreply.github.com>
2024-01-15 10:07:40 +1100

    Update putspreadsheet.py changed the sign for CALLS

--- putspreadsheet.py
+++ putspreadsheet.py
@@ -61,7 +61,7 @@ def find_10_delta_row(optionType, ticker,data,dt_string,friday,today):
         if(optionType == -1):
             df_closest = my_df[my_df["delta"]>= -0.15]
         if(optionType == 1):
-           df_closest = my_df[my_df["delta"]>= 0.15]
+           df_closest = my_df[my_df["delta"]<= 0.15]
         return df_closest.iloc[0]
 
     except Exception as e: print("Exception is ", e)
