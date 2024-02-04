341eecebc7e5d78d0214880088a6f1b72674cf22
sdesirazu <99233525+sdesirazu@users.noreply.github.com>
2024-02-04 15:23:44 +1100

    Update putspreadsheet.py trying fullmatch

--- putspreadsheet.py
+++ putspreadsheet.py
@@ -21,7 +21,7 @@ my_columns = ['currentPrice','strike','ticker','bid','ask','lastPrice','openInte
 
 def earnings(ticker):
   for key in earnings_partition_days.keys():
-    mask = earnings_partition_days[key]['Ticker'].str.contains(ticker,regex=False)
+    mask = earnings_partition_days[key]['Ticker'].str.fullmatch("r'"+ticker+"'")
     if(mask.any()):
       return ("Yes")
   return ("No")
